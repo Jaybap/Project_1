99f32f77cccff5dc72da1622566255ed3bfc10dd
Jaybap <jaybap@eden.rutgers.edu>
2013-08-08 15:36:37 -0400

    Moved incoming connection messages back to incoming controller to separate it from the peer object

--- Incoming_Controller.java
+++ Incoming_Controller.java
@@ -1,29 +1,45 @@
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.BitSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /*
- * This class takes care of incoming connections from peers,
- * checks peerlist, and reads the handshake from new peers.
- * 
- * @author Alex DeOliveira  [126-00-8635]
- * @author Jason Baptista   [126-00-4630]
- * @author Elizabeth Sanz   [127-00-8628]
- * @version "project01"
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
-
-public class Incoming_Controller {
+/**
+ *
+ * @author jaybap
+ */
+public class Incoming_Controller extends Thread {
 
     private boolean am_alive = false;
     private ServerSocket incoming;
     private int port;
     TorrentInfo torrent;
     RUBTClient client;
+    private boolean peer_is_interested = false;
+    private boolean keepalive_sent = false;
+    private boolean receiveBitfield=false;
+    private boolean isClientchoking=true;
+    public BitSet peerbitfield = null;
+    
+    final  int KEY_CHOKE = 0;
+    final  int KEY_UNCHOKE = 1;
+    final  int KEY_INTERESTED = 2;
+    final  int KEY_UNINTERESTED = 3;
+    final  int KEY_HAVE = 4;
+    final  int KEY_BITFIELD = 5;
+    final  int KEY_REQUEST = 6;
+    final  int KEY_PIECE = 7;
+    final  int KEY_CANCEL = 8;
+    final  int KEY_PORT = 9;
 
     Incoming_Controller(Tracker tracker, TorrentInfo torr, RUBTClient c) {
 
@@ -33,7 +49,6 @@ public class Incoming_Controller {
         this.client = c;
     }
 
-    /* Spins the Incoming Controller Thread */
     public void run() {
 
         try {
@@ -52,51 +67,181 @@ public class Incoming_Controller {
                 /**
                  * Checks if the peer is in our peer list
                  */
-                if (!isValid(ip.getHostAddress())) {
-                    System.err.print("Peer from invalid IP address " + ip.getHostAddress() + "trying to connect");
-                } else {
-                    DataInputStream frompeer = new DataInputStream(peersocket.getInputStream());
+                DataInputStream frompeer = new DataInputStream(peersocket.getInputStream());
+                DataOutputStream topeer = new DataOutputStream(peersocket.getOutputStream());
 
-                    /**
-                     * Reads in the handshake response from new peer
-                     */
-                    byte[] response = new byte[68];
-                    frompeer.read(response);
+                /**
+                 * Reads in the handshake response from new peer
+                 */
+                byte[] response = new byte[68];
+                frompeer.read(response);
 
-                    boolean match = true;
-                    for (int y = 1; y <= 20; y++) {
-                        if (response[response.length - 20 - y] != torrent.info_hash.array()[torrent.info_hash.array().length - y]) {
-                            System.err.print("INFO HASHES DO NOT MATCH. INCOMING CONNECTION BEING CLOSED!");
-                            peersocket.close();
-                            match = false;
-                            break;
-                        }
+                boolean match = true;
+                for (int y = 1; y <= 20; y++) {
+                    if (response[response.length - 20 - y] !return null;= torrent.info_hash.array()[torrent.info_hash.array().length - y]) {
+                        System.err.print("INFO HASHES DO NOT MATCH. INCOMING CONNECTION BEING CLOSED!");
+                        peersocket.close();
+                        match = false;
+                        break;
                     }
+                }
 
-                    if (match) {
-                        String response_string = new String(response);
-                        String peer_id = response_string.substring(48);
-                        Peer peer = new Peer(ip.toString(), port, true, peer_id);
+                if (match) {
+                    String response_string = new String(response);
+                    String peer_id = response_string.substring(48);
+
+                    Handshake(RUBTClient.peerID, RUBTClient.torrent.info_hash);
+                    Messages.sendBitfield(RUBTClient.Bitfield, peersocket, topeer);
+
+                    setpoint:
+                    try {
+                        if (frompeer.available() > 0) { //TODO: check if it runs
+
+                            keepalive_sent = false;
+
+                            int len = getPeerResponseInt(peersocket, topeer, frompeer);
+                            if (len != 0) {
+
+                                byte id = Messages.getPeerResponseByte(peersocket, topeer, frompeer);
+
+                                //if message received is not bitfield msg then sets the flag to false
+                                if (id != KEY_BITFIELD) {
+                                    receiveBitfield = false;
+                                }
+
+                                switch (id) {
+                                    case KEY_CHOKE:
+
+                                        break setpoint;
+
+                                    case KEY_UNCHOKE:
+
+                                        break setpoint;
+
+                                    case KEY_INTERESTED:
+                                        if (Messages.unchoke(peersocket, topeer, frompeer)) {
+                                            isClientchoking=false;
+                                            peer_is_interested = true;
+                                            System.out.println("Peer " + peer_id + " sent an interested message");
+                                        } else {
+                                            System.err.println("Peer " + peer_id + " sent illegal interested message. Violated Protocol so closing connection.");
+                                            terminateSocketConnections(peersocket, topeer, frompeer);
+                                        }
+                                        break;
+
+                                    case KEY_UNINTERESTED:
+                                        peer_is_interested = false;
+                                        break;
+
+                                    case KEY_HAVE:
+                                        Messages.receiveHave(peersocket, topeer, frompeer,peerbitfield , peer_id);
+                                        break setpoint;
+
+                                    case KEY_BITFIELD:
+                                        //if bitfield flag is set
+                                        if (receiveBitfield) {
+                                            try {
+                                               peerbitfield= Messages.receiveBitField(len,peer_id, peersocket,topeer,frompeer);
+
+                                            } catch (Exception ex) {
+                                                Logger.getLogger(Peer.class.getName()).log(Level.SEVERE, null, ex);
+                                            }
+                                        } else {
+                                            //else bitfield was received out of order and hence close connection
+                                            System.out.println("Received bitfield out of sync... closing connection");
+                                            this.suicide();
+                                        }
+                                        break;
+
+                                    case KEY_REQUEST:
+                                        if (isClientchoking) {
+                                            System.err.println("Peer " + peer_id + " sent a request message while choked. Violated Protocol and so closing connection.");
+                                            this.suicide();
+                                            break;
+                                        }
+
+                                        int index = getPeerResponseInt(peersocket, topeer, frompeer);
+                                        int offset = getPeerResponseInt(peersocket, topeer, frompeer);
+                                        int length = getPeerResponseInt(peersocket, topeer, frompeer);
+                                        System.out.println("Request received from peer " + peer_id + " for i=" + index + " o=" + offset);
+                                        
+                                            byte[] block = new byte[length];
+                                            System.arraycopy(RUBTClient.piecesDL[index].toByteArray(), offset, block, 0, length);
+                                        
+                                        Messages.piece(peersocket, topeer,index, offset, block);
+                                        break;
+
+                                    case KEY_PIECE:
+
+                                        break setpoint;
+
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Caught IOException: " + e.getMessage());
+                        /*
+                         * Restarts incoming controller to listen to more conections from peers
+                         */
+                        run();
+
                     }
                 }
             }
-        } catch (IOException e) {
-            System.err.println("Caught IOException: " + e.getMessage());
-            /*
-             * Restarts incoming controller to listen to more conections from peers
-             */
-            run();
-
+        } catch (IOException ex) {
+            Logger.getLogger(Incoming_Controller.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 
-    /* Method: Suicide, used to set am_alive false */
     public void suicide() {
         am_alive = false;
     }
 
-    private boolean isValid(String ip) {
-        return ip.equals("128.6.171.3") || ip.equals("128.6.171.4");
+    public byte[] getPeerResponse(int length, Socket peersocket, DataOutputStream topeer, DataInputStream frompeer) {
+        try {
+            byte[] peerResponse = new byte[length];
+            frompeer.readFully(peerResponse);
+
+            return peerResponse;
+        } catch (IOException e) {
+            terminateSocketConnections(peersocket, topeer, frompeer);
+            System.err.println("ERROR: Unable to receive peer response. ");
+            return null;
+        } catch (Exception e) {
+            System.err.println("ERROR: Unable to receive peer response. ");
+            return null;
+        }
+    }
+
+    public static void terminateSocketConnections(Socket peerSocket, DataOutputStream client2peer, DataInputStream peer2client) {
+        try {
+            if (!peerSocket.isClosed()) {
+                peerSocket.close();
+                client2peer.close();
+                peer2client.close();
+            }
+        } catch (Exception e) {
+            System.err.println("ERROR: Could not terminate open socket connections. ");
+        }
+    }
+
+    /**
+     * Method: get int response
+     */
+    public synchronized int getPeerResponseInt(Socket peersocket, DataOutputStream topeer, DataInputStream frompeer) {
+        try {
+            return frompeer.readInt();
+        } catch (IOException e) {
+            terminateSocketConnections(peersocket, topeer, frompeer);
+            System.out.println("first " + e.toString());
+            System.err.println("ERROR: Unable to receive peer response. ");
+            return -1;
+        } catch (Exception e) {
+            System.out.println("second " + e.toString());
+            System.err.println("ERROR: Unable to receive peer response. ");
+            return -1;
+        }
     }
 }
