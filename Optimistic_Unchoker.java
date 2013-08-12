e5b6502aae4634291ee0de110a767cb4602410d1
Jaybap <jaybap@eden.rutgers.edu>
2013-08-12 19:23:35 -0400

    Update Optimistic_Unchoker.java

--- Optimistic_Unchoker.java
+++ Optimistic_Unchoker.java
@@ -27,21 +27,21 @@ public class Optimistic_Unchoker extends Thread {
             int index;
             if (RUBTClient.uploadconnections[0].upload_rate < RUBTClient.uploadconnections[1].upload_rate && RUBTClient.uploadconnections[1].upload_rate < RUBTClient.uploadconnections[2].upload_rate) {
                 index = 0;
-                RUBTClient.uploadconnections[index].sendchoke;
+                RUBTClient.uploadconnections[index].sendchoke(RUBTClient.uploadconnections[index].peersocket,RUBTClient.uploadconnections[index].topeer,RUBTClient.uploadconnections[index].frompeer);
                 RUBTClient.uploadconnections[index] = null;
             } else if (RUBTClient.uploadconnections[0].upload_rate>RUBTClient.uploadconnections[1].upload_rate && RUBTClient.uploadconnections[1].upload_rate < RUBTClient.uploadconnections[2].upload_rate) {
                 index = 1;
-                RUBTClient.uploadconnections[index].sendchoke;
+                RUBTClient.uploadconnections[index].sendchoke(RUBTClient.uploadconnections[index].peersocket,RUBTClient.uploadconnections[index].topeer,RUBTClient.uploadconnections[index].frompeer);
                 RUBTClient.uploadconnections[index] = null;
             } else {
-                RUBTClient.uploadconnections[index].sendchoke;
+                RUBTClient.uploadconnections[index].sendchoke(RUBTClient.uploadconnections[index].peersocket,RUBTClient.uploadconnections[index].topeer,RUBTClient.uploadconnections[index].frompeer);
                 index = 2;
                 RUBTClient.uploadconnections[index] = null;
             }
 
             int peertoadd = randomGenerator.nextInt(RUBTClient.chokedpeers.size());
             RUBTClient.uploadconnections[index] = RUBTClient.chokedpeers.get(peertoadd);
-            RUBTClient.uploadconnections[index].sendunchoke;
+            RUBTClient.uploadconnections[index].sendunchoke(RUBTClient.uploadconnections[index].peersocket,RUBTClient.uploadconnections[index].topeer,RUBTClient.uploadconnections[index].frompeer);
 
         }
     }
