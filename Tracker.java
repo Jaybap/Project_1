a6f1002f192d3f883fd0420e7a0a47d960b8a7ee
esanz91 <esanz91@gmail.com>
2013-08-14 10:27:17 -0400

    Update Tracker.java
    cleaned

--- Tracker.java
+++ Tracker.java
@@ -1,104 +1,92 @@
-
 import java.io.*;
 import java.util.*;
 import java.net.*;
 import java.nio.*;
 
 /**
- * Tracker Class Main Functions: 1) Store Tracker Information 2) Connect Client
- * to Tracker through socket 3) Connect Client to Tracker through HTTP
- * Connection 4) Receive Tracker Response 5) Decode Tracker Response 6) Extract
- * Interval 7) Extract Peers 8) Create an ArrayList of Peers (in this case we
- * are only connecting to one)
- *
+ * Tracker Class Main Functions: 
+ * 1) Store Tracker Information 
+ * 2) Connect Client to Tracker through socket 
+ * 3) Connect Client to Tracker through HTTP Connection 
+ * 4) Receive Tracker Response 
+ * 5) Decode Tracker Response 
+ * 6) Extract Interval 
+ * 7) Extract Peers 
+ * 8) Create an ArrayList of Peers (in this case we are only connecting to one)
  */
 public class Tracker extends Thread{
 
-    /**
-     * The Constant requestSize
-     */
+    /*  The Constant requestSize */
     public static final int requestSize = 16000;
-    public static final char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    public static final char[] HEX_CHARS = 
+    {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         'A', 'B', 'C', 'D', 'E', 'F'};
-    /**
-     * Torrent Information: infoHash announceURL torrent_file_bytes piece_length
-     * piece_hashes
-     */
+    
+    /* Torrent Information: infoHash announceURL torrent_file_bytes piece_length
+       piece_hashes */
     private static TorrentInfo torrentData;
-    /**
-     * Client Information: destinationFile, bytesDownloaded, bytesUploaded,
-     * bytesRemaining, event;
-     */
+    
+    /* Client Information: destinationFile, bytesDownloaded, bytesUploaded,
+       bytesRemaining, event; */
     private static RUBTClient client;
-    /**
-     * Tracker Information
-     */
+    
+    /* Tracker Information */
     private static URL trackerUrl;
     private static String trackerIP;
     private static int trackerPort;
     private static int trackerInterval;
     private static boolean am_alive;
-    /**
-     * Connection Information
-     */
+    
+    /* Connection Information */
     private static URL requestedURL;
     private static ArrayList<String> peerIPList = new ArrayList<String>();
     static int listeningPort = -1;
-    /**
-     * keyINTERVAL
-     */
+   
+    /* keyINTERVAL */
     public static final ByteBuffer keyINTERVAL =
             ByteBuffer.wrap(new byte[]{'i', 'n', 't', 'e', 'r', 'v', 'a', 'l'});
-    /**
-     * keyPEERS
-     */
+    
+    /* keyPEERS */
     public static final ByteBuffer keyPEERS =
             ByteBuffer.wrap(new byte[]{'p', 'e', 'e', 'r', 's'});
-    /**
-     * keyPEER_IP
-     */
+    
+    /* keyPEER_IP */
     public static final ByteBuffer keyPEER_IP =
             ByteBuffer.wrap(new byte[]{'i', 'p'});
-    /**
-     * keyPEER_ID
-     */
+    
+    /* keyPEER_ID */
     public static final ByteBuffer keyPEER_ID =
             ByteBuffer.wrap(new byte[]{'p', 'e', 'e', 'r', ' ', 'i', 'd'});
-    /**
-     * keyPEER_PORT
-     */
+    
+    /* keyPEER_PORT */
     public static final ByteBuffer keyPEER_PORT =
             ByteBuffer.wrap(new byte[]{'p', 'o', 'r', 't'});
+    
+    /* keyFAILURE */
     public static ByteBuffer keyFAILURE =
             ByteBuffer.wrap(new byte[]{'f', 'a', 'i', 'l', 'u', 'r', 'e', ' ', 'r', 'e', 'a', 's', 'o', 'n'});
 
     /* ================================================================================ */
-    /*     							Tracker Constructor									*/
+    /*  Tracker Constructor								*/
     /* ================================================================================ */
     Tracker(RUBTClient c, TorrentInfo t) {
 
-        /**
-         * Fill in Client Information
-         */
-        client = c; 										/* RUBTClient */
+        /* Fill in Client Information */
+        client = c; 			
 
-        /**
-         * Fill in Client Information
-         */
-        torrentData = t; 									/* TorrentInfo */
-
-        /**
-         * Fill in Tracker Information
-         */
-        trackerUrl = torrentData.announce_url; 				/* URL */
-        trackerIP = trackerUrl.getHost(); 					/* String */
-        trackerPort = trackerUrl.getPort(); 				/* int */
+        /* Fill in Client Information */
+        torrentData = t; 		 
+        
+        /* Fill in Tracker Information */
+        trackerUrl = torrentData.announce_url; 				
+        trackerIP = trackerUrl.getHost(); 					
+        trackerPort = trackerUrl.getPort(); 				
         am_alive = true;
         start();
     }
 
     /**
-     * Starts up a Tracker thread that sleeps for the tracker interval amount of
+     * Method: Starts up a Tracker thread that sleeps for the tracker interval amount of
      * time and then contacts the tracker to get an updated peers list
      */
     public void run() {
@@ -119,34 +107,37 @@ public class Tracker extends Thread{
     }
 
     /* ================================================================================ */
-    /* 									METHODS  										*/
+    /* METHODS                                                                          */
     /* ================================================================================ */
+    
+    /**
+     * Method: Creates a connection with Tracker.
+     *
+     * @param bytesDown The amount of bytes downloaded
+     * @param bytesUp The amount of bytes uploaded
+     * @param bytesRemaining The amount of bytes remaining
+     * @param event The state of download
+     * @return a Map of the tracker's response
+     * 
+     */
     public synchronized Map connect(int bytesDown, int bytesUp, int bytesRemaining, String event) throws IOException {
-
-        /**
-         * Variables
-         */
+        
+        /* Variables */
         Socket trkSocket = null;
         URL trkURL = null;
         HttpURLConnection trkConnection = null;
-
         DataInputStream trackerData;
         int size;
-
         byte[] trkDataByteArray = null;
         Map<ByteBuffer, Object> trkMapResponse = null;
 
-        /**
-         * Verify Tracker was initialized
-         */
+        /* Verify Tracker was initialized */
         if (trackerUrl == null) {
             System.err.println("Tracker was not created properly. ");
             return null;
         }
 
-        /**
-         * Open socket in order to communicate with tracker
-         */
+        /* Open socket in order to communicate with tracker */
         try {
             trkSocket = new Socket(trackerIP, trackerPort);
         } catch (Exception e) {
@@ -154,9 +145,7 @@ public class Tracker extends Thread{
             return null;
         }
 
-        /**
-         * Create tracker HTTP URL connection
-         */
+        /* Create tracker HTTP URL connection */
         try {
             trkURL = newURL(bytesDown, bytesUp, bytesRemaining, trackerUrl);
             trkConnection = (HttpURLConnection) trkURL.openConnection();
@@ -165,9 +154,7 @@ public class Tracker extends Thread{
             return null;
         }
 
-        /**
-         * Receiving tracker response
-         */
+        /* Receiving tracker response */
         try {
             trackerData = new DataInputStream(trkConnection.getInputStream());
             size = trkConnection.getContentLength();
@@ -180,9 +167,7 @@ public class Tracker extends Thread{
             return null;
         }
 
-        /**
-         * Decoding tracker byte Array response to Map
-         */
+        /* Decoding tracker byte Array response to Map */
         try {
             trkMapResponse = (Map<ByteBuffer, Object>) Bencoder2.decode(trkDataByteArray);
             if (trkMapResponse.get(keyFAILURE) != null) {
@@ -192,14 +177,10 @@ public class Tracker extends Thread{
             System.err.println("Unable to decode tracker response. ");
         }
 
-        /**
-         * Extract and set Info from tracker response
-         */
+        /* Extract and set Info from tracker response */
         setpeerIPList(trkMapResponse);
 
-        /**
-         * Close Socket
-         */
+        /* Close Socket */
         if (trkSocket != null) {
             try {
                 trkSocket.close();
@@ -211,7 +192,8 @@ public class Tracker extends Thread{
     }
 
     /**
-     * Method: set peer list
+     * Method: set peer list 
+     * @param trackerResponse the response of tracker
      */
     public static void setpeerIPList(Map<ByteBuffer, Object> trackerResponse) {
 
@@ -219,23 +201,13 @@ public class Tracker extends Thread{
         String[] decodedTrkResponse;
         String delims = "[:]";
 
-        /**
-         * Extract interval
-         */
+        /* Extract interval */
         trackerInterval = (Integer) trackerResponse.get(keyINTERVAL);
         System.out.println("trackerInterval: " + trackerInterval);
 
-        /**
-         * Decode tracker Map response to String[]
-         */
+        /* Decode tracker Map response to String[] */
         decodedTrkResponse = decodeCompressedPeers(trackerResponse);
 
-        /**
-         * Extract SPECIFIC peer
-         */
-        /* ================ */
-        /* Print Statements */
-        /* ================ */
         System.out.println("NumPeers: " + decodedTrkResponse.length);
 
         for (int i = 0; i < decodedTrkResponse.length; i++) {
@@ -247,19 +219,20 @@ public class Tracker extends Thread{
 
     /**
      * Method: Create and return requested URL
+     * @param bytesDown The amount of bytes downloaded
+     * @param bytesUp The amount of bytes uploaded
+     * @param bytesRemaining The amount of bytes remaining
+     * @param announceURL URL extracted from parsed torrent file
+     * 
      */
     public static URL newURL(int bytesDown, int bytesUp, int bytesRemaining, URL announceURL) {
         /* Variables */
         String newUrlString = "";
 
-        /**
-         * Find a random port to connect
-         */
+        /* Find a random port to connect */
         listeningPort = setPortNum();
 
-        /**
-         * Create requestedURL
-         */
+        /*  Create requestedURL  */
         newUrlString += trackerUrl
                 + "?info_hash=" + toHexString(torrentData.info_hash.array())
                 + "&peer_id=" + toHexString((client.getPeerId()).getBytes())
@@ -272,9 +245,7 @@ public class Tracker extends Thread{
             newUrlString += "&event=" + (client.getEvent());
         }
 
-        /**
-         * Return requested URL
-         */
+        /* Return requested URL */
         try {
             requestedURL = new URL(newUrlString);
             return requestedURL;
@@ -286,8 +257,8 @@ public class Tracker extends Thread{
 
     /**
      * Method: Turn bytes to HexStrings
+     * @param bytes 
      */
-    /* NOTE: NEED TO MODIFY */
     public static String toHexString(byte[] bytes) {
         if (bytes == null) {
             return null;
@@ -310,6 +281,8 @@ public class Tracker extends Thread{
 
     /**
      * Method: Decode Map to String[]
+     * @param map the tracker response 
+     * @return String[] decoded array of peers from tracker response
      */
     public static String[] decodeCompressedPeers(Map<ByteBuffer, Object> map) {
         ByteBuffer peers = (ByteBuffer) map.get(ByteBuffer.wrap("peers".getBytes()));
@@ -331,7 +304,7 @@ public class Tracker extends Thread{
     }
 
     /* ================================================================================ */
-    /* 									SET-METHODS  									*/
+    /* SET-METHODS  									*/
     /* ================================================================================ */
     /**
      * Method: Returns a port to connect on
@@ -355,24 +328,39 @@ public class Tracker extends Thread{
     }
 
     /* ================================================================================ */
-    /* 									GET-METHODS  									*/
+    /* GET-METHODS  									*/
     /* ================================================================================ */
+     /**
+     * Method: Returns an array list of peers
+     */
     public ArrayList<String> getpeerIPList() {
         return peerIPList;
     }
 
+     /**
+     * Method: Returns tracker url
+     */
     public URL getTrackerUrl() {
         return trackerUrl;
     }
 
+     /**
+     * Method: Returns a tracker ip address
+     */
     public String getTrackerIP() {
         return trackerIP;
     }
 
+     /**
+     * Method: Returns a port to tracker
+     */
     public int getTrackerPort() {
         return trackerPort;
     }
 
+     /**
+     * Method: Returns a tracker interval
+     */
     public int getTrackerInterval() {
         return trackerInterval;
     }
