c1493080a9d79c0dc80d8f22f1cb21bd21a8bf07
esanz91 <esanz91@gmail.com>
2013-08-14 10:42:45 -0400

    Update Optimistic_Unchoker.java
    cleaned

--- Optimistic_Unchoker.java
+++ Optimistic_Unchoker.java
@@ -3,16 +3,23 @@ import java.util.logging.Logger;
 import java.util.Random;
 
 /**
- *
- * @author jaybap
+ * Optimistic_Unchoker Class Main Functions: 
+ * 1) Choke all but 3 peers based on download rate
+ * 
+ * @author Alex DeOliveira [126-00-8635]
+ * @author Jason Baptista [126-00-4630]
+ * @author Elizabeth Sanz [127-00-8628]
+ * @version "project02"
  */
 public class Optimistic_Unchoker extends Thread {
 
+    /* Global variable */
     private boolean am_alive = false;
 
+    
     public Optimistic_Unchoker() {
         this.am_alive = true;
-        this.start;
+        this.start();
     }
 
     public void run() {
@@ -23,29 +30,29 @@ public class Optimistic_Unchoker extends Thread {
                 Logger.getLogger(Optimistic_Unchoker.class.getName()).log(Level.SEVERE, null, ex);
             }
 
-
             int index;
-            if (RUBTClient.uploadconnections[0].upload_rate < RUBTClient.uploadconnections[1].upload_rate && RUBTClient.uploadconnections[1].upload_rate < RUBTClient.uploadconnections[2].upload_rate) {
+            if (RUBTClient.uploadconnections[0].upload_rate < RUBTClient.uploadconnections[1].upload_rate 
+                    && RUBTClient.uploadconnections[1].upload_rate < RUBTClient.uploadconnections[2].upload_rate) {
                 index = 0;
                 RUBTClient.uploadconnections[index].sendchoke(RUBTClient.uploadconnections[index].peersocket,RUBTClient.uploadconnections[index].topeer,RUBTClient.uploadconnections[index].frompeer);
                 RUBTClient.uploadconnections[index] = null;
-                RUBTClient.choked_peers.add(RUBTClient.uploadconnections[index]);
-            } else if (RUBTClient.uploadconnections[0].upload_rate>RUBTClient.uploadconnections[1].upload_rate && RUBTClient.uploadconnections[1].upload_rate < RUBTClient.uploadconnections[2].upload_rate) {
+            } 
+            else if (RUBTClient.uploadconnections[0].upload_rate>RUBTClient.uploadconnections[1].upload_rate 
+                    && RUBTClient.uploadconnections[1].upload_rate < RUBTClient.uploadconnections[2].upload_rate) {
                 index = 1;
                 RUBTClient.uploadconnections[index].sendchoke(RUBTClient.uploadconnections[index].peersocket,RUBTClient.uploadconnections[index].topeer,RUBTClient.uploadconnections[index].frompeer);
                 RUBTClient.uploadconnections[index] = null;
-                RUBTClient.choked_peers.add(RUBTClient.uploadconnections[index]);
-            } else {
+            } 
+            else {
                 index = 2;
                 RUBTClient.uploadconnections[index].sendchoke(RUBTClient.uploadconnections[index].peersocket,RUBTClient.uploadconnections[index].topeer,RUBTClient.uploadconnections[index].frompeer);
                 RUBTClient.uploadconnections[index] = null;
-                RUBTClient.choked_peers.add(RUBTClient.uploadconnections[index]);
             }
 
-            int peertoadd = randomGenerator.nextInt(RUBTClient.chokedpeers.size());
-            RUBTClient.uploadconnections[index] = RUBTClient.chokedpeers.get(peertoadd);
+            Random randomGenerator = new Random();
+            int peertoadd = randomGenerator.nextInt(RUBTClient.choked_peers.size());
+            RUBTClient.uploadconnections[index] = RUBTClient.choked_peers.get(peertoadd);
             RUBTClient.uploadconnections[index].sendunchoke(RUBTClient.uploadconnections[index].peersocket,RUBTClient.uploadconnections[index].topeer,RUBTClient.uploadconnections[index].frompeer);
-
         }
     }
 }
